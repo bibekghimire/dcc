bb3addabe81e06826facdeefcf23ff7734c09e58
Bibek Ghimire <bibekghimire99@gmail.com>
2024-09-11 19:39:28 +0545

    setteled static files and directories

--- annapurna/settings.py
+++ annapurna/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'annapurna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'annapurna',
-        'USER':'python',
-        'PASSWORD':' ',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'annapurna',
+    #     'USER':'python',
+    #     'PASSWORD':' ',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
