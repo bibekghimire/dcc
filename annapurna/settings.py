08a76e82710e1edda5f46e3c95295b90c9198536
Bibek Ghimire <bibekghimire99@gmail.com>
2024-09-12 08:25:41 +0545

    commit for deployment

--- annapurna/settings.py
+++ annapurna/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'annapurna.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'annapurna',
-    #     'USER':'python',
-    #     'PASSWORD':' ',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'annapurna',
+        'USER':'python',
+        'PASSWORD':' ',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
