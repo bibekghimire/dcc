14a83b971c48bc836ea5ad7d2178aa3b70118102
Bibek Ghimire <bibekghimire99@gmail.com>
2024-10-03 06:48:39 +0545

    environments changes

--- annapurna/settings.py
+++ annapurna/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j7+36+f+b93wp+s3cgn64=)z4b!1m@dwm9w@vmhe@4&@$2=x#5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.0.110']
 
@@ -83,11 +83,19 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'annapurna_db',
+    #     'USER':'annapurna',
+    #     'PASSWORD':'Abc@1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'annapurna_db',
-        'USER':'annapurna',
-        'PASSWORD':'Abc@1234',
+        'NAME': 'annapurna',
+        'USER':'python',
+        'PASSWORD':' ',
         'HOST': 'localhost',
         'PORT': '3306',
     }
